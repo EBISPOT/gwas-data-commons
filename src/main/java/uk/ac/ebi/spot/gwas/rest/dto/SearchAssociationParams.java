b5f622f0e9161bf290d6a9a0c35f71a1c9334c70
Sajo <sajo@ebi.ac.uk>
2025-05-09 17:49:59 +0100

    goci-rest-71-rest-api-documentation adding definition of fields in documentation

--- src/main/java/uk/ac/ebi/spot/gwas/rest/dto/SearchAssociationParams.java
+++ src/main/java/uk/ac/ebi/spot/gwas/rest/dto/SearchAssociationParams.java
@@ -10,31 +10,39 @@ import uk.ac.ebi.spot.gwas.annotation.GWASParamName;
 public class SearchAssociationParams {
 
     @GWASParamName("pubmed_id")
-    @Parameter(name = "pubmed_id")
+    @Parameter(name = "pubmed_id" , description = "pubmedId of the publication <br/> <br/>" +
+            "<i> Example </i> : 35241825")
     String pubmedId = null;
 
     @GWASParamName("rs_id")
-    @Parameter(name = "rs_id")
+    @Parameter(name = "rs_id" ,description = "Strongest SNP; if a haplotype it may include more than one" +
+            " rs number (multiple SNPs comprising the haplotype) <br/> <br/>" +
+            "<i> Example </i> : rs3093017 ")
     String rsId = null;
 
     @GWASParamName("full_pvalue_set")
-    @Parameter(name = "full_pvalue_set")
+    @Parameter(name = "full_pvalue_set",  description = "Whether full summary statistics are available for this study <br/> <br/>" +
+            "<i> Example </i> : true/false")
     Boolean fullPvalueSet = null;
 
     @GWASParamName("accession_id")
-    @Parameter(name = "accession_id")
+    @Parameter(name = "accession_id", description = "The study’s GWAS Catalog accession ID <br/> <br/>" +
+            "<i> Example </i> : GCST000854")
     String accessionId = null;
 
     @GWASParamName("efo_trait")
-    @Parameter(name = "efo_trait")
+    @Parameter(name = "efo_trait",  description = "The trait name or label <br/> <br/>" +
+            "<i> Example </i> : celiac disease")
     String efoTrait = null;
 
     @GWASParamName("efo_id")
-    @Parameter(name = "efo_id")
+    @Parameter(name = "efo_id", description = "The trait URI shortform <br/> <br/>" +
+            "<i> Example </i> : EFO_0001060")
     String shortForm = null;
 
     @GWASParamName("show_child_trait")
-    @Parameter(name = "show_child_trait")
+    @Parameter(name = "show_child_trait" , description = "display entities for descendants of a parent Efo Trait Term <br/> <br/>" +
+            "<i> Example </i> : true/false")
     Boolean showChildTrait = null;
 
 }
--- src/main/java/uk/ac/ebi/spot/gwas/rest/dto/SearchSnpParams.java
+++ src/main/java/uk/ac/ebi/spot/gwas/rest/dto/SearchSnpParams.java
@@ -10,27 +10,39 @@ import uk.ac.ebi.spot.gwas.annotation.GWASParamName;
 public class SearchSnpParams {
 
     @GWASParamName("rs_id")
-    @Parameter(name = "rs_id")
+    @Parameter(name = "rs_id", description = "Strongest SNP; if a haplotype it may include more than one" +
+            " rs number (multiple SNPs comprising the haplotype) <br/> <br/>" +
+            "<i> Example </i> : rs3093017 ")
     String rsId = null;
 
     @GWASParamName("bp_location")
-    @Parameter(name = "bp_location")
+    @Parameter(name = "bp_location" , description = "search for SNPs via their base pair location <br/> <br/>" +
+            "<i> Example </i> : 95000000")
     Integer bpLocation = null;
 
     @GWASParamName("bp_start")
-    @Parameter(name = "bp_start")
+    @Parameter(name = "bp_start", description = "Search for SNPs on a certain chromosome " +
+            "and within a given base pair location range <br/> <br/>" +
+                     "<i> Example </i> : 95000000")
     Long bpStart = null;
 
     @GWASParamName("bp_end")
-    @Parameter(name = "bp_end")
+    @Parameter(name = "bp_end", description = "Search for SNPs on a certain chromosome " +
+            "and within a given base pair location range <br/> <br/>" +
+            "<i> Example </i> : 95000000")
     Long bpEnd = null;
 
     @GWASParamName("pubmed_id")
-    @Parameter(name = "pubmed_id")
+    @Parameter(name = "pubmed_id", description = "pubmedId of the publication <br/> <br/>" +
+            "<i> Example </i> : 35241825")
     String pubmedId = null;
 
+    @Parameter(name = "chromosome", description = "Search for SNPs on a certain chromosome <br/> <br/>" +
+            "<i> Example </i> : 13")
     String chromosome = null;
 
+    @Parameter(name = "gene", description = "Search for SNPs via a gene using parameter geneName <br/> <br/>" +
+            "<i> Example </i> : 13")
     String gene =  null;
 
 }
